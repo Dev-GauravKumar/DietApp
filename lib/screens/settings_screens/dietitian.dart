37f87b1c3583a6213e74cc95cb0405f18cae1a51
Gaurav Kumar <gaurav.kumar@fitelo.co>
2023-05-28 13:32:50 +0530

    bio data screen fixed

--- lib/screens/settings_screens/dietitian.dart
+++ lib/screens/settings_screens/dietitian.dart
@@ -14,103 +14,106 @@ class DietitianBio extends StatelessWidget {
       extendBody: true,
       resizeToAvoidBottomInset: true,
       appBar: simpleAppBar(context, title: 'Dietitian Biodata'),
-      body: SizedBox(
-        height: _size.height * 0.56,
-        width: _size.width,
-        child: Image.network(
-          'https://img.freepik.com/free-photo/asian-female-doctor-physician-medical-uniform-with-stethoscope-cross-arms-chest-smiling-looking-like-professional-white-background_1258-83205.jpg?w=996&t=st=1683124508~exp=1683125108~hmac=b466a99dabddb96e8afca6161f774cf26eaa0f0b5901ba63266ea7baf68dc443',
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          SizedBox(
+            height: _size.height,
+            width: _size.width,
+            child: Image.network(
+              'https://img.freepik.com/free-photo/asian-female-doctor-physician-medical-uniform-with-stethoscope-cross-arms-chest-smiling-looking-like-professional-white-background_1258-83205.jpg?w=996&t=st=1683124508~exp=1683125108~hmac=b466a99dabddb96e8afca6161f774cf26eaa0f0b5901ba63266ea7baf68dc443',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+              height: _size.height * 0.35,
+              width: _size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  topLeft: Radius.circular(20),
+                ),
+                color: AppColors.searchBoxBgColor,
+              ),
+              child: Padding(
+                padding:
+                    EdgeInsets.symmetric(vertical: 20.sp, horizontal: 5.sp),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Dr. Jane Doe',
+                      style: AppTextStyles.s22w600black
+                          .copyWith(fontWeight: FontWeight.w700),
+                    ),
+                    VerticalSizeBox(height: 5.sp),
+                    Text(
+                      'Dietitian',
+                      style: AppTextStyles.s18w500white
+                          .copyWith(color: AppColors.blackColor),
+                    ),
+                    VerticalSizeBox(height: 15.sp),
+                    Text(
+                      'Biography',
+                      style: AppTextStyles.s18w700black,
+                    ),
+                    VerticalSizeBox(height: 10.sp),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                      style: AppTextStyles.s14w500cloginFieldValue
+                          .copyWith(color: AppColors.blackColor),
+                    ),
+                    VerticalSizeBox(height: 30.sp),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: 40,
+                          width: 170,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0.0,
+                              primary: AppColors.loginPageBgColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            ),
+                            child: Text(
+                              'Book Appointment',
+                              style: AppTextStyles.s16w500black.copyWith(
+                                  fontSize: 16.sp,
+                                  color: AppColors.loginFieldValueColor),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                          width: 160,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0.0,
+                              primary: AppColors.loginPageTitleColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            ),
+                            child: Text(
+                              'Message',
+                              style: AppTextStyles.s16w500black.copyWith(
+                                  fontSize: 16.sp,
+                                  color: AppColors.loginFieldValueColor),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ],
       ),
-      bottomSheet: SizedBox(
-          height: _size.height * 0.35,
-          width: _size.width,
-          child: Card(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20),
-                topLeft: Radius.circular(20),
-              ),
-            ),
-            elevation: 0.0,
-            color: AppColors.searchBoxBgColor,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.sp, horizontal: 5.sp),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Dr. Jane Doe',
-                    style: AppTextStyles.s22w600black
-                        .copyWith(fontWeight: FontWeight.w700),
-                  ),
-                  VerticalSizeBox(height: 5.sp),
-                  Text(
-                    'Dietitian',
-                    style: AppTextStyles.s18w500white
-                        .copyWith(color: AppColors.blackColor),
-                  ),
-                  VerticalSizeBox(height: 15.sp),
-                  Text(
-                    'Biography',
-                    style: AppTextStyles.s18w700black,
-                  ),
-                  VerticalSizeBox(height: 10.sp),
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                    style: AppTextStyles.s14w500cloginFieldValue
-                        .copyWith(color: AppColors.blackColor),
-                  ),
-                  VerticalSizeBox(height: 30.sp),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        height: 40,
-                        width: 170,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0.0,
-                            primary: AppColors.loginPageBgColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          ),
-                          child: Text(
-                            'Book Appointment',
-                            style: AppTextStyles.s16w500black.copyWith(
-                                fontSize: 16.sp,
-                                color: AppColors.loginFieldValueColor),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40,
-                        width: 160,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0.0,
-                            primary: AppColors.loginPageTitleColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          ),
-                          child: Text(
-                            'Message',
-                            style: AppTextStyles.s16w500black.copyWith(
-                                fontSize: 16.sp,
-                                color: AppColors.loginFieldValueColor),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )),
     );
   }
 }
